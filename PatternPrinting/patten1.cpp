44962ba892597e30024cf6bdfd85efdab16acb3b
Harish Kumar V <166478902+Harish200510@users.noreply.github.com>
2025-12-19 13:28:01 +0530

    Update patten1.cpp

--- PatternPrinting/patten1.cpp
+++ PatternPrinting/patten1.cpp
@@ -1,15 +1,26 @@
+step1:
+   ->The number of row times run the outer loop
+   no.of lines or rows==now of times outer loop runs
+step2:
+    ->Identify the every row numbers
+    ->how many cols are there
+    ->Identify what they are asking to print star or numbers
+
+pattern:
+
 * * * *
 * * * *
 * * * *
 * * * *
 
 code:
+
 #include <bits/stdc++.h>
 using namespace std;
 void pattern1(int n){
     
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n;col++){
             cout<<"* ";
         }
         cout<<endl;
