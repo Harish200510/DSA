5109cc4d50eda145a85305c0aba1352dbb1a3b9d
Harish Kumar V <166478902+Harish200510@users.noreply.github.com>
2025-12-19 13:23:17 +0530

    Fix formatting and clean up pattern1.cpp

---
+++ PatternPrinting/patten1.cpp
@@ -0,0 +1,21 @@
+* * * *
+* * * *
+* * * *
+* * * *
+
+code:
+#include <bits/stdc++.h>
+using namespace std;
+void pattern1(int n){
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+int main() {
+    pattern1(4);
+   
+}
