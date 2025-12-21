be6b68a6fb9b48f84f3cbd97abf8c57576feefb7
Harish Kumar V <166478902+Harish200510@users.noreply.github.com>
2025-12-21 22:36:31 +0530

    Update pattern5.cpp

--- PatternPrinting/pattern5.cpp
+++ PatternPrinting/pattern5.cpp
@@ -12,4 +12,20 @@ pattern:
 
 code:
 
+#include <bits/stdc++.h>
+using namespace std;
+void pattern5(int n){
+    
+    for(int row=1;row<(2*n);row++){
+        int totcols=row<=n?row:(n-(row%n));
+        for(int col=1;col<=totcols;col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+int main() {
+   pattern5(5);
+    return 0;
+}
   
