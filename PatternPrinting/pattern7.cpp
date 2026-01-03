3e5d5f7dc3830cc7272b36a3a2bdf3481699740b
Harish Kumar V <166478902+Harish200510@users.noreply.github.com>
2026-01-03 09:01:19 +0530

    Update pattern7.cpp

--- PatternPrinting/pattern7.cpp
+++ PatternPrinting/pattern7.cpp
@@ -32,3 +32,44 @@ int main() {
     return 0;
 }
 
+
+Pattern:
+
+        1 
+      2 1 2 
+    3 2 1 2 3 
+  4 3 2 1 2 3 4 
+5 4 3 2 1 2 3 4 5 
+  4 3 2 1 2 3 4 
+    3 2 1 2 3 
+      2 1 2 
+        1 
+
+code:
+#include <bits/stdc++.h>
+using namespace std;
+void pattern7(int n){
+    
+     for(int row=1;row<(n*2);row++){
+         
+          int totspace=row<=n?n-row:row%n;
+          for(int space=1;space<=totspace;space++){
+              cout<<"  ";
+          }
+          
+          int col=n-totspace;
+          for(int cols=col;cols>=1;cols--){
+              cout<<cols<<" ";
+          }
+          for(int cols=2;cols<=col;cols++){
+              cout<<cols<<" ";
+          }
+          cout<<endl;
+     }
+}
+int main() {
+   pattern7(5);
+    return 0;
+}
+
+
